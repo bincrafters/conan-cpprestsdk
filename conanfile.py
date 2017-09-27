0266907d9493a8ee10afed9bb6b2b013cc0a5ec3
SSE4 <tomskside@gmail.com>
2017-09-27 17:48:18 +0700

    - magic to get FindBoost working (tests on Mac)

--- conanfile.py
+++ conanfile.py
@@ -1,5 +1,7 @@
+from __future__ import print_function
 from conans import ConanFile, CMake, tools
-from os import path
+from os import path, getcwd
+import fnmatch
 
 
 class CppRestSDKConan(ConanFile):
@@ -33,9 +35,39 @@ class CppRestSDKConan(ConanFile):
         tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
 
     def build(self):
+        boost_include_dirs = []
+        boost_library_dirs = []
+        boost_libraries = []
+        for (pkg_name, cpp_info) in self.deps_cpp_info.dependencies:
+            if fnmatch.fnmatch(pkg_name, "Boost.*"):
+                boost_include_dirs.extend([path.join(cpp_info.rootpath, d) for d in cpp_info.includedirs])
+                if cpp_info.libs:
+                    boost_library_dirs.extend([path.join(cpp_info.rootpath, d) for d in cpp_info.libdirs])
+                    boost_libraries.extend(cpp_info.libs)
+
+        boost_include_dirs = ";".join(boost_include_dirs)
+        boost_library_dirs = ";".join(boost_library_dirs)
+        boost_libraries = ";".join(boost_libraries)
+
+        # we have to use our own FindBoost.cmake, as CMake's one does not support modular boost
+        with open("FindBoost.cmake", "w") as boost_config:
+            boost_config.write('message(STATUS "using boost config")\n')
+            boost_config.write('set(Boost_INCLUDE_DIRS "%s")\n' % boost_include_dirs)
+            boost_config.write('set(Boost_LIBRARY_DIRR "%s")\n' % boost_library_dirs)
+            boost_config.write('set(Boost_LIBRARIES "%s")\n' % boost_libraries)
+            boost_config.write('set(Boost_FOUND ON)\n')
+            for (pkg_name, cpp_info) in self.deps_cpp_info.dependencies:
+                if fnmatch.fnmatch(pkg_name, "Boost.*") and cpp_info.libs:
+                    for library in cpp_info.libs:
+                        library_name = library.split('_')[1].upper()
+                        boost_config.write('set(Boost_%s_LIBRARY "%s")\n' % (library_name, library))
+
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTS"] = False
         cmake.definitions["BUILD_SAMPLES"] = False
+        cmake.definitions["BUILD_SAMPLES"] = False
+        cmake.definitions["CMAKE_MODULE_PATH"] = getcwd()
+        cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = True
         cmake.configure()
         cmake.build()
 
