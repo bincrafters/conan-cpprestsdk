54a96d97384c26d14769743f07f665073c184afe
SSE4 <tomskside@gmail.com>
2017-09-30 21:28:56 +0700

    - another attempt to reduce travis log size

--- conanfile.py
+++ conanfile.py
@@ -16,6 +16,7 @@ class CppRestSDKConan(ConanFile):
     author = "Uilian Ries <uilianries@gmail.com>"
     description = "A project for cloud-based client-server communication in native code using a modern asynchronous C++ API design"
     license = "https://github.com/Microsoft/cpprestsdk/blob/master/license.txt"
+    root = "%s-%s" % (name, version)
 
     def requirements(self):
         self.requires.add("OpenSSL/1.0.2l@conan/stable")
@@ -64,6 +65,7 @@ class CppRestSDKConan(ConanFile):
 
                         boost_config.write('set(Boost_%s_LIBRARY "%s")\n' % (library_name, library))
 
+        tools.replace_in_file(os.path.join(self.root, 'Release', 'CMakeLists.txt'), '-Wconversion', '-Wno-conversion')
         cmake = CMake(self)
         cmake.definitions["BUILD_TESTS"] = False
         cmake.definitions["BUILD_SAMPLES"] = False
@@ -72,7 +74,7 @@ class CppRestSDKConan(ConanFile):
         cmake.definitions["CMAKE_VERBOSE_MAKEFILE"] = True
         cmake.definitions["WERROR"] = False
         if self.settings.os != "Windows":
-            cmake.definitions["CMAKE_CXX_FLAGS"] = "-Wno-conversion"
+            cmake.definitions["CMAKE_CXX_FLAGS"] = ""
         if self.settings.os == "iOS":
             cmake.definitions["IOS"] = True
         elif self.settings.os == "Android":
@@ -81,14 +83,13 @@ class CppRestSDKConan(ConanFile):
         cmake.build()
 
     def package(self):
-        root = "%s-%s" % (self.name, self.version)
-        self.copy("license.txt",  dst=".", src=root)
-        self.copy(pattern="*", dst="include", src=path.join(root, "Release", "include"))
+        self.copy("license.txt",  dst=".", src=self.root)
+        self.copy(pattern="*", dst="include", src=path.join(self.root, "Release", "include"))
         self.copy(pattern="*.dll", dst="bin", src="bin", keep_path=False)
         self.copy(pattern="*.lib", dst="lib", src="lib", keep_path=False)
         self.copy(pattern="*.a", dst="lib", src="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", src=path.join(root, "Release", "Binaries"), keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", src=path.join(root, "Release", "Binaries"), keep_path=False)
+        self.copy(pattern="*.so*", dst="lib", src=path.join(self.root, "Release", "Binaries"), keep_path=False)
+        self.copy(pattern="*.dylib", dst="lib", src=path.join(self.root, "Release", "Binaries"), keep_path=False)
 
     def package_info(self):
         version_tokens = self.version.split(".")
