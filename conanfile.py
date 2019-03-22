d7455eab30e1a98c3068454fd397c7a957ec6262
Uilian Ries <uilianries@gmail.com>
2019-03-22 11:33:37 -0300

    Bump version to 2.10.11

--- conanfile.py
+++ conanfile.py
@@ -14,7 +14,7 @@ def find_sysroot(sdk):
 
 class CppRestSDKConan(ConanFile):
     name = "cpprestsdk"
-    version = "2.10.10"
+    version = "2.10.11"
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
     options = {
